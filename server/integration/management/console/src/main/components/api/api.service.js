89619fe5316ebec486b986c3c4d0d7d447a25c3a
Martin Sosic <sosic.martin@gmail.com>
2014-10-19 16:39:39 +0200

    Moved data fetching to api service

--- server/integration/management/console/src/main/components/api/api.service.js
+++ server/integration/management/console/src/main/components/api/api.service.js
@@ -13,18 +13,59 @@ angular.module('managementConsole.api')
 
       var domain = new DomainModel(dmrClient);
 
-      var getClusters = function(callback) {
+      /**
+       * Fetches all clusters, but in order to get more data you need to refresh them manually.
+       * @param callback([ClusterModel]) Callback whose first param is list of clusters.
+       */
+      var getClustersShallow = function(callback) {
         domain.refresh(function(d) {
           callback(d.getClusters());
         });
       };
 
+
+      /**
+       * Fetches all clusters and all their data, including all nodes and caches.
+       * @param callback([ClusterModel]) Callback whose first param is list of clusters.
+       */
+      var getClustersDeep = function(callback) {
+        var clusters = undefined;
+
+        var jobsInProgress = 0;
+        var jobStarted = function() {
+          jobsInProgress += 1;
+        };
+        var jobEnded = function() {
+          jobsInProgress -= 1;
+          if (jobsInProgress === 0) {
+            // TODO(martinsos): Although it should not happen,
+            // it is theoretically possible that callback will be called multiple times.
+            callback(clusters);
+          }
+        };
+
+        domain.refresh(function(domain) {
+          clusters = domain.getClusters();
+          angular.forEach(clusters, function(cluster) {
+            jobStarted();
+            cluster.refresh(function(cluster) {
+              // Refresh caches.
+              var caches = cluster.getCaches();
+              angular.forEach(caches, function(cache) {
+                jobStarted();
+                cache.refresh(function() {
+                  jobEnded();
+                });
+              });
+              jobEnded();
+            });
+          });
+        });
+      };
+
       return {
-        /**
-         * Fetches all clusters.
-         * @param callback([ClusterModel]) Callback whose first param is list of clusters.
-         */
-        getClusters: getClusters
+        getClustersShallow: getClustersShallow,
+        getClustersDeep: getClustersDeep
       };
     }
   ]);
--- server/integration/management/console/src/main/webapp/cluster-view/cluster-view.controller.js
+++ server/integration/management/console/src/main/webapp/cluster-view/cluster-view.controller.js
@@ -11,34 +11,11 @@ angular.module('managementConsole')
       $scope.clusters = undefined;
       $scope.currentCluster = undefined;
 
-      var jobsInProgress = 0;
-      var jobStarted = function() {
-        jobsInProgress += 1;
-      };
-      var jobEnded = function() {
-        jobsInProgress -= 1;
-        if (jobsInProgress === 0) {
-          $scope.$digest();
-        }
-      };
       // Fetch all clusters and their caches.
-      api.getClusters(function(clusters) {
-        $scope.clusters = clusters;
-        $scope.currentCluster = clusters[0];
-        console.log(clusters);
-        angular.forEach(clusters, function(cluster) {
-          jobStarted();
-          cluster.refresh(function(cluster) {
-            // Refresh caches.
-            var caches = cluster.getCaches();
-            angular.forEach(caches, function(cache) {
-              jobStarted();
-              cache.refresh(function() {
-                jobEnded();
-              });
-            });
-            jobEnded();
-          });
+      api.getClustersDeep(function(clusters) {
+        $scope.$apply(function() {
+          $scope.clusters = clusters;
+          $scope.currentCluster = clusters[0];
         });
       });
   }]);
